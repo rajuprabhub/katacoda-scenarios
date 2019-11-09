5ad43663ea946a2bf74c32b0b3d3e474bc45576f
Balakrishna, Rajagopal <rajagopal.balakrishn@rakuten.com>
2019-11-09 13:38:17 +0530

    adding background script

--- rajlearnk8s/background.sh
+++ rajlearnk8s/background.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 touch /root/createdFromBackgroundScript
+./startUp.sh
---
+++ rajlearnk8s/startUp.sh
@@ -0,0 +1,9 @@
+echo Waiting for Kubernetes to start...
+  while [ ! -f /root/.kube/config ]
+  do
+    sleep 1
+  done
+echo Kubernetes started
+if [ -f /root/.kube/start ]; then
+  /root/.kube/start
+fi
